315c08ddd2728267a65954decc8643fe5eaf5ecf
pranay-lamse <pranay.l@veerit.com>
2025-05-24 14:56:08 +0530

    Init

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import { Button, Result } from 'antd';
+
+import useLanguage from '@/locale/useLanguage';
+
+const About = () => {
+  const translate = useLanguage();
+  return <>Comming Soon..</>;
+};
+
+export default About;
